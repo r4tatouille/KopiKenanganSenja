9ae6570976a0e256c66e33c2a2be516555f84c9b
Rheiya <rheiya@example.com>
2025-02-16 01:25:13 +0700

    add some new changes at style and js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// Toggle
+// Toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+
+// Ketika menu di klik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
