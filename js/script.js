4600f1e1311a63c78c2810a2fdfae8bd35d2e0ee
Rheiya <rheiya@example.com>
2025-04-11 14:29:46 +0700

    add changes in javascript

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ const navbarnav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
+
+//Klik di luar side bar untuk menghilangkan navbar pada tampilah mobile
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
